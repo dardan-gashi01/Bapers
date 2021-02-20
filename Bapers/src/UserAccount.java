26acc89aefc17a8a9398cc9576c8ee2ec0889619
dardan-gashi01 <58520384+dardan-gashi01@users.noreply.github.com>
2021-02-20 19:48:13 +0000

    creation of project

---
+++ Bapers/src/UserAccount.java
@@ -0,0 +1,12 @@
+public class UserAccount {
+
+    String role;
+    String username;
+    String password;
+
+    public UserAccount(String role, String username, String password){
+        this.role = role;
+        this.username = username;
+        this.password = password;
+    }
+}
