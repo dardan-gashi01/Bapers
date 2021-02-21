14dfb7aa095ce918c0067559bdb3ff705a57cbee
dardan-gashi01 <58520384+dardan-gashi01@users.noreply.github.com>
2021-02-21 14:22:06 +0000

    main menu layout
    made the layout but have not changed it to make it look like the GUI yet

--- Bapers/src/Menu.java
+++ Bapers/src/Menu.java
@@ -1,5 +1,29 @@
 import javax.swing.*;
 
-public class Menu extends JFrame {
+public class Menu extends JFrame{
 
+    private JPanel PanelMain;
+    private JButton manageCustomersButton;
+    private JButton reportsButton;
+    private JButton restoreDatabaseButton;
+    private JButton createBackupButton;
+    private JButton updateAccountButton;
+    private JButton createAccountButton;
+    private JButton viewActiveJobsButton;
+    private JButton createCustomerButton;
+    private JButton viewAllJobsButton;
+    private JButton updateJobStatusButton;
+    private JButton takePaymentButton;
+    private JButton placeJobButton;
+    private JButton logoutButton;
+
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Bapers System");
+        frame.setContentPane(new Menu().PanelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
 }
